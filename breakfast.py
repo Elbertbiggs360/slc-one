e1878724101b301203375dce667c0d73cb665f2b
unknown <brucebigirwenkya@gmail.com>
2017-04-17 15:43:22 +0300

    Initial Commit

---
+++ breakfast.py
@@ -0,0 +1,13 @@
+"""Breakfast"""
+
+print("Get the ingredients")
+print("Mix the ingredients")
+print("Pour the mixture into the heated pan")
+print("flip the mixture to the other side")
+print("Check for readiness")
+print("Take the made egg off the pan and onto serving plate")
+print("Add extra ingredients")
+print("Add ingredients for tea to cup")
+print("stir ingredients")
+print("Serve plate and cup contents")
+print("---Breakfast Served---")
