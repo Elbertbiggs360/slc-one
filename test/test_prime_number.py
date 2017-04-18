b5941930c50458d9a6a5b9714af4bfcf8c5cfb5b
unknown <brucebigirwenkya@gmail.com>
2017-04-18 12:38:12 +0300

    Added tests to evaluate the outputs as prime numbers

--- test/test_prime_number.py
+++ test/test_prime_number.py
@@ -6,6 +6,9 @@ class PrimeNumberTest(unittest.TestCase):
 	
 	def setUp(self):
 		self.prime_number = Prime_number()
+		self.arg_limit = 32
+        self.sample_prime = [2,3,5,7,911,13,17,19,23,29,31]
+        self.sample_prime_length = 11
 
 	def test_function_returns_integers(self):
 		"""Check that the function only returns integers """
@@ -26,18 +29,19 @@ class PrimeNumberTest(unittest.TestCase):
 	def test_function_returns_value_error_if_arg_less_than_three(self):
 		self.assertRaises(ValueError('Input Number Greater than 2'), self.prime_number.compute_prime_numbers(), 1)
 
+	def test_generate_prime_numbers_returns_all_prime_numbers_less_than_arg(self):
+        self.assertEqual(self.sample_prime_length, len(self.prime_number.compute_prime_numbers(self.arg_limit)))
+
 	def test_function_returns_prime_numbers(self):
-		result = self.prime_number.compute_prime_numbers(15)
-		test_value = False
-		for i in result:
-			for n in len(round(math.sqrt(i)))
-				if n%2!=0 or n%3!=0 or n%5!=0:
-					if i%n==0:
-						test_value = False
-					else:
-						test_value = True
-		
-		self.assertTrue(test_value)
+		isPrime = True
+        for x in self.prime_number.compute_prime_numbers(self.arg_limit):
+            sqrt_value = sqrt(x)
+            sample_range = [i for i in self.sample_prime if i <= sqrt_value]
+            for y in sample_range:
+                if x%y == 0:
+                    isPrime = False
+                    break
+        self.assertTrue(isPrime)
 
 
 if __name__ = '__main__':
